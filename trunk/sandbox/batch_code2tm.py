c48b3f795565a86cd7bebced9b8f32e24f047e46
matteo.bertini <matteo.bertini@5ff7f63e-6833-0410-825b-1303a3858bc7>
2007-11-24 15:22:40 +0000

    Cleanup an reorganize code

--- trunk/sandbox/batch_code2tm.py
+++ trunk/sandbox/batch_code2tm.py
@@ -17,33 +17,32 @@ def get_frags_from_txt(code):
         tmpfrags = get_frags_from_txt(icode)
         for ifrag in [i for i in tmpfrags if i != ""]:
             frags = frags + tmpfrags
-#    frags = frags.append("")
     return frags
 
-EXCLUDE_LIST = ["perceptron/pyml_perceptron.py",]
-perceptron = [i for i in glob.glob("perceptron/*.py") if i not in EXCLUDE_LIST]
+def process_files(filelist):
+    for srcfile in filelist:
+        sys.stdout.write("Processing %s... \n" % srcfile)
+        sys.stdout.flush()
+        for frag in get_frags_from_file(srcfile):
+            if frag:
+                outname = srcfile + "." + frag.replace(" ","") + ".tm"
+            else:
+                outname = srcfile + ".tm"            
+                try:
+                    if outname not in ["perceptron/perceptron.py.tm","perceptron/svg_paths.py.tm"]:
+                        tmsource = tmcolorizer.get_tmsource(srcfile, frag)
+                        tmfile = file("./tm/%s" % outname, "w+")
+                        tmfile.write(tmsource)
+                        tmfile.close()
+                        if __debug__:
+                            sys.stdout.write(".. %s\n" % frag)
+                            sys.stdout.flush()
+                except tmcolorizer.EmptyFragmentError:
+                    pass
+                sys.stdout.write("*** Done! ***\n")    
 
-alberi = glob.glob("alberi/*.py")
+def main():
+    process_files(glob.glob("*.py"))
 
-filelist = glob.glob("*.py")
-
-for srcfile in filelist:
-    sys.stdout.write("Processing %s... " % srcfile)
-    sys.stdout.flush()
-    for frag in get_frags_from_file(srcfile):
-        if frag:
-            outname = srcfile + "." + frag.replace(" ","") + ".tm"
-        else:
-            outname = srcfile + ".tm"            
-        try:
-            if outname not in ["perceptron/perceptron.py.tm","perceptron/svg_paths.py.tm"]:
-                tmsource = tmcolorizer.get_tmsource(srcfile, frag)
-                tmfile = file("./tm/%s" % outname, "w+")
-                tmfile.write(tmsource)
-                tmfile.close()
-            if __debug__:
-                sys.stdout.write(".. %s\n" % frag)
-                sys.stdout.flush()
-        except tmcolorizer.EmptyFragmentError:
-            pass
-    sys.stdout.write("*** Done! ***\n")    
+if __name__ == "__main__":
+    main()
