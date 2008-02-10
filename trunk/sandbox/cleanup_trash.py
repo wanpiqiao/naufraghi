8e8249d6695baaff4ff9318cfad47393074db320
matteo.bertini <matteo.bertini@5ff7f63e-6833-0410-825b-1303a3858bc7>
2008-02-10 09:13:07 +0000

    Use optparse

--- trunk/sandbox/cleanup_trash.py
+++ trunk/sandbox/cleanup_trash.py
@@ -10,17 +10,14 @@ HOUR = 60 * MINUTE
 DAY  = 24 * HOUR
 WEEK =  7 * DAY
 
-rtime = 2 * WEEK
-
 MEGA = 1024 * 1024
-
-msize = 10 * MEGA
 
 
 def getDirSize(dirname):
     res = 0
     for root, _, files in os.walk(dirname):
         res += sum([os.path.getsize(os.path.join(root, f)) for f in files if os.path.isfile(f)])
+    print dirname, res
     return res
 
 def getContentsStats(basepath):
@@ -34,18 +31,29 @@ def getContentsStats(basepath):
     return [(gsize(i), max(os.stat(i)[-3:]), i) for i in ls(".")]
 
 if __name__ == "__main__":
-    #os.chdir("/Users/matteo/.Trash/")
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option("-r", "--rtime", dest="rtime", default=2, type="int",
+                      help="Oldness hint in weeks, default = %default")
+    parser.add_option("-m", "--msize", dest="msize", default=10, type="int",
+                      help="Bigness hint in Mb, default = %default")
+    parser.add_option("-d", "--delete",
+                      action="store_true", dest="delete", default=False,
+                      help="Delete old/big files, default = %default")
+    (options, args) = parser.parse_args()
+
+    os.chdir(os.path.expanduser("~/.Trash"))
     stats = getContentsStats(".")
     trash_list = sorted([(size * mtime, name) for size, mtime, name in stats], reverse=True)
-    border_value = msize * (time.time() - rtime)
+    border_value = options.msize * MEGA * (time.time() - options.rtime * WEEK)
     remove_list = [name for v, name in trash_list if v > border_value]
     print "#"*40
-    print "# rtime = %d Weeks" % (rtime / WEEK)
-    print "# msize = %d Mb" % (msize / MEGA)
+    print "# rtime = %d Weeks" % options.rtime
+    print "# msize = %d Mb" % options.msize
     print "#"*40
     for name in remove_list:
         print os.path.isdir(name) and "[ DIR]" or "[FILE]",
-        if "--delete" in sys.argv:
+        if options.delete:
             print "REMOVING...",
             if os.path.isdir(name):
                 shutil.rmtree(name)
