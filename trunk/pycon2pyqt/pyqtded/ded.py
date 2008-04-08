eb65fbb94f7c9dc3ec6d922174b087f3694854f3
lmancini80 <lmancini80@5ff7f63e-6833-0410-825b-1303a3858bc7>
2008-04-08 23:58:56 +0000

    Add very simple d&d demo

---
+++ trunk/pycon2pyqt/pyqtded/ded.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import sys
+from PyQt4.Qt import *
+
+class DDTextEdit(QTextEdit):
+    def __init__(self, *args):
+        super(DDTextEdit, self).__init__(*args)
+        self.setAcceptDrops(True)
+        self.setReadOnly(True)
+
+    # Necessario accettare gli eventi di enter drag e move drag
+    def dragEnterEvent(self, event):
+        event.acceptProposedAction()
+
+    def dragMoveEvent(self, event):
+        event.acceptProposedAction()
+
+    def dropEvent(self, event):
+        event.acceptProposedAction()
+        
+        # L'evento di drop contiene le informazioni a proposito dell'oggetto
+        if event.mimeData().hasUrls():
+            self.setText('\n'.join([unicode(url.toLocalFile()) for url in event.mimeData().urls()]))
+
+class DedDemo(QWidget):
+    def __init__(self, *args):
+        super(DedDemo, self).__init__(*args)
+
+        self.setWindowTitle(self.tr("D&D Demo"))
+        
+        self.vl = QVBoxLayout(self)
+        self.l = QLabel(self.tr("Trascinate qua sotto i vostri file..."))
+        self.te = DDTextEdit(None)
+        self.vl.addWidget(self.l)
+        self.vl.addWidget(self.te)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ded = DedDemo()
+    ded.show()
+    sys.exit(app.exec_())
+
