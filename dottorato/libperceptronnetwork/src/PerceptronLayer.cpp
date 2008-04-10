c195178e8f5846ead27f1d739bdf2fdc993bd9de
matteo.bertini <matteo.bertini@5ff7f63e-6833-0410-825b-1303a3858bc7>
2008-04-10 14:29:56 +0000

    Tab -> 4 spaces

--- dottorato/libperceptronnetwork/src/PerceptronLayer.cpp
+++ dottorato/libperceptronnetwork/src/PerceptronLayer.cpp
@@ -13,196 +13,195 @@ using namespace std;
 
 
 PerceptronLayer::PerceptronLayer (unsigned int neuron_count,
-								  const ActivationFunction *fact)
+                                  const ActivationFunction *fact)
 {
-	this->fact = fact;
-	neurons = vector<PerceptronNeuron *> (neuron_count);
+    this->fact = fact;
+    neurons = vector<PerceptronNeuron *> (neuron_count);
 
-	for (unsigned int n = 0 ; n < neuron_count ; ++n)
-		neurons[n] = new PerceptronNeuron (n);
+    for (unsigned int n = 0 ; n < neuron_count ; ++n)
+        neurons[n] = new PerceptronNeuron (n);
 }
 
 
 PerceptronLayer::~PerceptronLayer (void)
 {
-	unsigned int	n;
+    unsigned int    n;
 
-	for (n = 0 ; n < neurons.size () ; ++n)
-		delete neurons[n];
+    for (n = 0 ; n < neurons.size () ; ++n)
+        delete neurons[n];
 }
 
 
 PerceptronLayer::PerceptronLayer (PerceptronLayer& source)
 {
-	type = source.type;
-	fact = source.fact;
+    type = source.type;
+    fact = source.fact;
 
-	for (unsigned int nn = 0 ; nn < source.neurons.size () ; ++nn)
-		neurons.push_back (new PerceptronNeuron (*source.neurons[nn]));
+    for (unsigned int nn = 0 ; nn < source.neurons.size () ; ++nn)
+        neurons.push_back (new PerceptronNeuron (*source.neurons[nn]));
 }
 
 
 void
 PerceptronLayer::randomizeParameters (PerceptronLayer *succ,
-									  const RandomFunction *weight_func,
-				                      const RandomFunction *theta_func)
+                                      const RandomFunction *weight_func,
+                                      const RandomFunction *theta_func)
 {
-	unsigned int	n,
-					wn;
+    unsigned int    n,
+                    wn;
 
-	for (n = 0 ; n < neurons.size () ; ++n) {
-		/* initialize theta value accordingly
-		 * note that Input type layers do not have a theta value.
-		 */
-		if (type != Input)
-			neurons[n]->setTheta (theta_func->func
-				(theta_func->user));
+    for (n = 0 ; n < neurons.size () ; ++n) {
+        /* initialize theta value accordingly
+         * note that Input type layers do not have a theta value.
+         */
+        if (type != Input)
+            neurons[n]->setTheta (theta_func->func
+                (theta_func->user));
 
-		/* if there is no succeeding layer, skip weight assignment
-		 */
-		if (succ == NULL)
-			continue;
+        /* if there is no succeeding layer, skip weight assignment
+         */
+        if (succ == NULL)
+            continue;
 
-		/* now all weighted connections to neurons in the succeeding
-		 * layer
-		 */
-		for (wn = 0 ; wn < succ->neurons.size () ; ++wn) {
-			neurons[n]->weight[wn] = weight_func->func (weight_func->user);
-		}
-	}
+        /* now all weighted connections to neurons in the succeeding
+         * layer
+         */
+        for (wn = 0 ; wn < succ->neurons.size () ; ++wn) {
+            neurons[n]->weight[wn] = weight_func->func (weight_func->user);
+        }
+    }
 }
 
 
 void
 PerceptronLayer::resetDiffs (void)
 {
-	for (unsigned int n = 0 ; n < neurons.size () ; ++n)
-		neurons[n]->resetDiffs ();
+    for (unsigned int n = 0 ; n < neurons.size () ; ++n)
+        neurons[n]->resetDiffs ();
 }
 
 
 void
 PerceptronLayer::setActivationFunction (const ActivationFunction *fact)
 {
-	this->fact = fact;
+    this->fact = fact;
 }
 
 
 const ActivationFunction *
 PerceptronLayer::getActivationFunction (void) const
 {
-	return (fact);
+    return (fact);
 }
 
 
 void
 PerceptronLayer::propagate (PerceptronLayer *pred)
 {
-	double		input_sum;
-	unsigned int	nidx;
+    double      input_sum;
+    unsigned int    nidx;
 
-	/* first calculate all the inputs of this layer, then the neuron
-	 * activation. note that we never propagate an Input type layer.
-	 */
-	assert (type != Input);
+    /* first calculate all the inputs of this layer, then the neuron
+     * activation. note that we never propagate an Input type layer.
+     */
+    assert (type != Input);
 
-	for (nidx = 0 ; nidx < neurons.size () ; ++nidx) {
-		input_sum = 0.0;
+    for (nidx = 0 ; nidx < neurons.size () ; ++nidx) {
+        input_sum = 0.0;
 
-		/* sum up all (weight * output) products from the previous
-		 * layer
-		 */
-		for (unsigned int pn = 0 ; pn < pred->neurons.size () ; ++pn) {
-			input_sum += pred->neurons[pn]->weight[nidx] * pred->neurons[pn]->output;
-		}
+        /* sum up all (weight * output) products from the previous
+         * layer
+         */
+        for (unsigned int pn = 0 ; pn < pred->neurons.size () ; ++pn) {
+            input_sum += pred->neurons[pn]->weight[nidx] * pred->neurons[pn]->output;
+        }
 
-		neurons[nidx]->input = input_sum;
-	}
+        neurons[nidx]->input = input_sum;
+    }
 
-	/* now calculate the output of each neuron
-	 */
-	for (nidx = 0 ; nidx < neurons.size () ; ++nidx) {
-		neurons[nidx]->output = fact->normal (neurons[nidx]->input, neurons[nidx]->getTheta ());
-	}
+    /* now calculate the output of each neuron
+     */
+    for (nidx = 0 ; nidx < neurons.size () ; ++nidx) {
+        neurons[nidx]->output = fact->normal (neurons[nidx]->input, neurons[nidx]->getTheta ());
+    }
 }
 
 
 void
 PerceptronLayer::backpropagate (PerceptronLayer *succ,
-				                vector<double>& test_output,
-								double test_tolerance)
+                                vector<double>& test_output,
+                                double test_tolerance)
 {
-	unsigned int	nidx,
-					sn;
-	double		delta,
-				test_diff;
+    unsigned int    nidx,
+                    sn;
+    double      delta,
+                test_diff;
 
-	for (nidx = 0 ; nidx < neurons.size () ; ++nidx) {
-		/* an output-layer neuron
-		 */
-		if (type == Output) {
-			delta = fact->derivate (neurons[nidx]->input, neurons[nidx]->getTheta ());
+    for (nidx = 0 ; nidx < neurons.size () ; ++nidx) {
+        /* an output-layer neuron
+         */
+        if (type == Output) {
+            delta = fact->derivate (neurons[nidx]->input, neurons[nidx]->getTheta ());
 
-			test_diff = (test_output[nidx] - neurons[nidx]->output);
+            test_diff = (test_output[nidx] - neurons[nidx]->output);
 
-			/* when the difference is below the tolerance
-			 * treshhold, we just set it to zero.
-			 */
-			if (test_tolerance != 0.0 &&
-				test_diff >= (-1.0 * test_tolerance) &&
-				test_diff <= test_tolerance)
-			{
-				test_diff = 0.0;
-			}
+            /* when the difference is below the tolerance
+             * treshhold, we just set it to zero.
+             */
+            if (test_tolerance != 0.0 &&
+                test_diff >= (-1.0 * test_tolerance) &&
+                test_diff <= test_tolerance)
+            {
+                test_diff = 0.0;
+            }
 
-			delta *= test_diff;
-			delta *= -1.0;
-			neurons[nidx]->setDelta (delta);
+            delta *= test_diff;
+            delta *= -1.0;
+            neurons[nidx]->setDelta (delta);
 
-			continue;
-		}
+            continue;
+        }
 
-		/* a normal (non-output-layer) neuron
-		 *
-		 * sum up all weighted delta values of the successor neurons
-		 */
-		delta = 0.0;
-		for (sn = 0 ; sn < succ->neurons.size () ; ++sn) {
-			delta += neurons[nidx]->weight[sn] * succ->neurons[sn]->getDelta ();
-		}
-		delta *= fact->derivate (neurons[nidx]->input, neurons[nidx]->getTheta ());
+        /* a normal (non-output-layer) neuron
+         *
+         * sum up all weighted delta values of the successor neurons
+         */
+        delta = 0.0;
+        for (sn = 0 ; sn < succ->neurons.size () ; ++sn) {
+            delta += neurons[nidx]->weight[sn] * succ->neurons[sn]->getDelta ();
+        }
+        delta *= fact->derivate (neurons[nidx]->input, neurons[nidx]->getTheta ());
 
-		neurons[nidx]->setDelta (delta);
-	}
+        neurons[nidx]->setDelta (delta);
+    }
 }
 
 
 void
 PerceptronLayer::postprocess (PerceptronLayer *succ, double epsilon,
-				              double weight_decay, double momterm)
+                              double weight_decay, double momterm)
 {
-	unsigned int	n,
-					succ_n;
+    unsigned int    n,
+                    succ_n;
 
-	for (n = 0 ; n < neurons.size () ; ++n) {
-		neurons[n]->postprocessTheta (epsilon, weight_decay, momterm);
+    for (n = 0 ; n < neurons.size () ; ++n) {
+        neurons[n]->postprocessTheta (epsilon, weight_decay, momterm);
 
-		/* for each successor neuron, postprocess the weighting
-		 * differences.
-		 */
-		for (succ_n = 0 ; succ != NULL && succ_n < succ->neurons.size () ; ++succ_n)
-		{
-			neurons[n]->postprocessWeight (succ->neurons[succ_n], epsilon, weight_decay, momterm);
-		}
-	}
+        /* for each successor neuron, postprocess the weighting differences.
+         */
+        for (succ_n = 0 ; succ != NULL && succ_n < succ->neurons.size () ; ++succ_n)
+        {
+            neurons[n]->postprocessWeight (succ->neurons[succ_n], epsilon, weight_decay, momterm);
+        }
+    }
 }
 
 
 void
 PerceptronLayer::update (void)
 {
-	for (unsigned int n = 0 ; n < neurons.size () ; ++n)
-		neurons[n]->update ();
+    for (unsigned int n = 0 ; n < neurons.size () ; ++n)
+        neurons[n]->update ();
 }
 
 
--- dottorato/libperceptronnetwork/src/PerceptronNetwork.h
+++ dottorato/libperceptronnetwork/src/PerceptronNetwork.h
@@ -3,8 +3,8 @@
  * Copyright (C) 2002-2003 -- Sebastian Nowozin
  */
 
-#ifndef	PERCEPTRONNETWORK_H
-#define	PERCEPTRONNETWORK_H
+#ifndef PERCEPTRONNETWORK_H
+#define PERCEPTRONNETWORK_H
 
 #include <vector>
 #include <fstream>
@@ -18,243 +18,242 @@
 
 class
 PerceptronNetwork {
-	/** The GUI visualization class is declared friend to allow access to
-	 * individual layers.
-	 */
-	friend class WeightMatrix;
+    /** The GUI visualization class is declared friend to allow access to
+     * individual layers.
+     */
+    friend class WeightMatrix;
 
 public:
-	/** Symbolic name of the network. Never used by the methods
-	 * themselves, but convenient to use.
-	 */
-	const char *	name;
+    /** Symbolic name of the network. Never used by the methods
+     * themselves, but convenient to use.
+     */
+    const char *    name;
 
-	/** Input vector, fed into the first layer of the network. The number
-	 * of elements must equal the number of neurons within the input
-	 * layer.
-	 */
-	vector<double>	input;
+    /** Input vector, fed into the first layer of the network. The number
+     * of elements must equal the number of neurons within the input
+     * layer.
+     */
+    vector<double>  input;
 
-	/** Output vector, resulting from propagation through the entire
-	 * network. The number of elements equals the number of output neurons
-	 * within the network.
-	 */
-	vector<double>	output;
+    /** Output vector, resulting from propagation through the entire
+     * network. The number of elements equals the number of output neurons
+     * within the network.
+     */
+    vector<double>  output;
 
-	/** PerceptronNetwork constructor
-	 *
-	 * Construct a multilayer perceptron network by layer description
-	 * given through \a desc_layers. The vector size specifies the number
-	 * of layers, the individual elements the number of neurons within its
-	 * layer.
-	 *
-	 * @param desc_layers Number of neurons within each layer, given the
-	 * input layer as first and the output layer as last. Hence, the size
-	 * of the vector must be at least two.
-	 *
-	 * @param network_name Symbolic name of the network. Can be NULL. The
-	 * name is pointer-copied, so we steal a pointer here.
-	 *
-	 * @param fact Activation function to use for all neurons by default
-	 * within the network. By default, its the tangens-hyperbolicus
-	 * function. For individual layers, based one their type, the
-	 * activation function can be changed using the changeActivation
-	 * method.
-	 */
-	PerceptronNetwork (vector<unsigned int> desc_layers,
-		const char *network_name = "unnamed",
-		const ActivationFunction *fact =
-			&ActivationFunctions::fact_tanh);
+    /** PerceptronNetwork constructor
+     *
+     * Construct a multilayer perceptron network by layer description
+     * given through \a desc_layers. The vector size specifies the number
+     * of layers, the individual elements the number of neurons within its
+     * layer.
+     *
+     * @param desc_layers Number of neurons within each layer, given the
+     * input layer as first and the output layer as last. Hence, the size
+     * of the vector must be at least two.
+     *
+     * @param network_name Symbolic name of the network. Can be NULL. The
+     * name is pointer-copied, so we steal a pointer here.
+     *
+     * @param fact Activation function to use for all neurons by default
+     * within the network. By default, its the tangens-hyperbolicus
+     * function. For individual layers, based one their type, the
+     * activation function can be changed using the changeActivation
+     * method.
+     */
+    PerceptronNetwork (vector<unsigned int> desc_layers,
+                       const char *network_name = "unnamed",
+                       const ActivationFunction *fact = &ActivationFunctions::fact_tanh);
 
-	/* Default constructor. (To be used with the load method).
-	 */
-	PerceptronNetwork (void);
+    /* Default constructor. (To be used with the load method).
+     */
+    PerceptronNetwork (void);
 
-	/** PerceptronNetwork destructor
-	 *
-	 * Remove all layers stored within the network.
-	 */
-	~PerceptronNetwork (void);
+    /** PerceptronNetwork destructor
+     *
+     * Remove all layers stored within the network.
+     */
+    ~PerceptronNetwork (void);
 
-	/** Copy constructor.
-	 *
-	 * @param source Source object to be copied.
-	 */
-	PerceptronNetwork (PerceptronNetwork& source);
+    /** Copy constructor.
+     *
+     * @param source Source object to be copied.
+     */
+    PerceptronNetwork (PerceptronNetwork& source);
 
-	/** Set the activation function for layers, based on their type.
-	 *
-	 * @param type Type of layers that will use the new activation
-	 * function.
-	 * @param fact Activation function to use.
-	 */
-	void setActivationFunction (PerceptronLayerType type,
-		const ActivationFunction *fact);
+    /** Set the activation function for layers, based on their type.
+     *
+     * @param type Type of layers that will use the new activation
+     * function.
+     * @param fact Activation function to use.
+     */
+    void setActivationFunction (PerceptronLayerType type,
+                                const ActivationFunction *fact);
 
-	/* LOAD/SAVE methods */
-	/** Save the entire network as text into the stream \a fs.
-	 *
-	 * @param fs Stream (output) to save the network to.
-	 */
-	void save (fstream& fs) const;
+    /* LOAD/SAVE methods */
+    /** Save the entire network as text into the stream \a fs.
+     *
+     * @param fs Stream (output) to save the network to.
+     */
+    void save (fstream& fs) const;
 
-	/** Load the entire network from stream \a fs.
-	 *
-	 * @param fs Stream (input) to read the network from.
-	 *
-	 * @return True on success, false on failure.
-	 */
-	bool load (fstream& fs);
+    /** Load the entire network from stream \a fs.
+     *
+     * @param fs Stream (input) to read the network from.
+     *
+     * @return True on success, false on failure.
+     */
+    bool load (fstream& fs);
 
-	/* NETWORK operation methods */
+    /* NETWORK operation methods */
 
-	/** Randomize the variable network parameters, weightings and theta
-	 * values.
-	 *
-	 * @param weight_func Function to randomize weighting values.
-	 * @param theta_func Function to randomize theta parameters.
-	 */
-	void randomizeParameters (const RandomFunction *weight_func,
-		const RandomFunction *theta_func);
+    /** Randomize the variable network parameters, weightings and theta
+     * values.
+     *
+     * @param weight_func Function to randomize weighting values.
+     * @param theta_func Function to randomize theta parameters.
+     */
+    void randomizeParameters (const RandomFunction *weight_func,
+                              const RandomFunction *theta_func);
 
-	/** Setter for the entire network input.
-	 *
-	 * @param in Vector of input signal levels. The size must equal the
-	 * input layer neuron count.
-	 */
-	void setInput (vector<double>& in);
+    /** Setter for the entire network input.
+     *
+     * @param in Vector of input signal levels. The size must equal the
+     * input layer neuron count.
+     */
+    void setInput (vector<double>& in);
 
-	/** Setter for the requested network output.
-	 *
-	 * Must be used before any learning algorithm is called
-	 * (backpropagate).
-	 *
-	 * @param test Test network output for the current input.
-	 */
-	void setTestOutput (vector<double>& test);
+    /** Setter for the requested network output.
+     *
+     * Must be used before any learning algorithm is called
+     * (backpropagate).
+     *
+     * @param test Test network output for the current input.
+     */
+    void setTestOutput (vector<double>& test);
 
-	/** Getter for the entire network output.
-	 */
-	vector<double> getOutput (void) const;
+    /** Getter for the entire network output.
+     */
+    vector<double> getOutput (void) const;
 
-	/** Calculate the errorterm for the network
-	 *
-	 * The error value for the current output is calculated. The current
-	 * test output must be given prior to calling this function.
-	 *
-	 * The formula used is \f$E = \frac{1}{2} \sum_{i} \left(t_{p,i} -
-	 * y_{p,i}^{\lambda,L}\right)^2\f$.
-	 */
-	double errorTerm (void) const;
+    /** Calculate the errorterm for the network
+     *
+     * The error value for the current output is calculated. The current
+     * test output must be given prior to calling this function.
+     *
+     * The formula used is \f$E = \frac{1}{2} \sum_{i} \left(t_{p,i} -
+     * y_{p,i}^{\lambda,L}\right)^2\f$.
+     */
+    double errorTerm (void) const;
 
-	/** Reset all learned parameters.
-	 *
-	 * Call after an update has been made.
-	 */
-	void resetDiffs (void);
+    /** Reset all learned parameters.
+     *
+     * Call after an update has been made.
+     */
+    void resetDiffs (void);
 
-	/* ALGORITHMS */
+    /* ALGORITHMS */
 
-	/** Propagation algorithm for the entire network.
-	 *
-	 * The input levels to the network have to be set using the setInput()
-	 * method, before. Afterwards the output of the network can be
-	 * obtained using the getOutput() method.
-	 */
-	void propagate (void);
+    /** Propagation algorithm for the entire network.
+     *
+     * The input levels to the network have to be set using the setInput()
+     * method, before. Afterwards the output of the network can be
+     * obtained using the getOutput() method.
+     */
+    void propagate (void);
 
-	/** Backpropagation algorithm for the entire network.
-	 *
-	 * Calculate all delta error signals in every layer and their neurons.
-	 * Every neuron must have a proper input/output signal assigned, and
-	 * the test_output training target result is used for calculation.
-	 */
-	void backpropagate (void);
+    /** Backpropagation algorithm for the entire network.
+     *
+     * Calculate all delta error signals in every layer and their neurons.
+     * Every neuron must have a proper input/output signal assigned, and
+     * the test_output training target result is used for calculation.
+     */
+    void backpropagate (void);
 
-	/** Postprocess algorithm for the entire network.
-	 */
-	void postprocess (void);
+    /** Postprocess algorithm for the entire network.
+     */
+    void postprocess (void);
 
-	/** Update algorithm for the entire network.
-	 */
-	void update (void);
+    /** Update algorithm for the entire network.
+     */
+    void update (void);
 
-	/* DEBUG facilities */
-	/** Dump whole neural network as graph file in the GraphViz file
-	 * format (www.graphviz.org).
-	 *
-	 * @param filename Name of the file to write the graph data to.
-	 */
-	void dumpNetworkGraph (const char *filename) const;
+    /* DEBUG facilities */
+    /** Dump whole neural network as graph file in the GraphViz file
+     * format (www.graphviz.org).
+     *
+     * @param filename Name of the file to write the graph data to.
+     */
+    void dumpNetworkGraph (const char *filename) const;
 
-	/** Getter for the epsilon parameter.
-	 */
-	double getLearningParameter (void) const;
+    /** Getter for the epsilon parameter.
+     */
+    double getLearningParameter (void) const;
 
-	/** Setter for the epsilon parameter.
-	 *
-	 * @param epsilon Learn parameter, in the range of 0.0 to 0.5.
-	 */
-	void setLearningParameter (double epsilon);
+    /** Setter for the epsilon parameter.
+     *
+     * @param epsilon Learn parameter, in the range of 0.0 to 0.5.
+     */
+    void setLearningParameter (double epsilon);
 
-	/** Getter for the test tolerance parameter.
-	 */
-	double getTestTolerance (void) const;
+    /** Getter for the test tolerance parameter.
+     */
+    double getTestTolerance (void) const;
 
-	/** Setter for the test tolerance parameter.
-	 *
-	 * @param tolerance Value between 0.0 and 0.2.
-	 */
-	void setTestTolerance (double tolerance);
+    /** Setter for the test tolerance parameter.
+     *
+     * @param tolerance Value between 0.0 and 0.2.
+     */
+    void setTestTolerance (double tolerance);
 
-	/** Getter for the weight decay parameter.
-	 */
-	double getWeightDecayParameter (void) const;
+    /** Getter for the weight decay parameter.
+     */
+    double getWeightDecayParameter (void) const;
 
-	/** Setter for the weight decay parameter.
-	 *
-	 * @param factor Value in the range of 0.00005 to 0.0001.
-	 */
-	void setWeightDecayParameter (double factor);
+    /** Setter for the weight decay parameter.
+     *
+     * @param factor Value in the range of 0.00005 to 0.0001.
+     */
+    void setWeightDecayParameter (double factor);
 
-	/** Getter for the momentum term parameter.
-	 */
-	double getMomentumTermParameter (void) const;
+    /** Getter for the momentum term parameter.
+     */
+    double getMomentumTermParameter (void) const;
 
-	/** Setter for the momentum term parameter.
-	 *
-	 * @param factor Value between 0.5 and 0.9.
-	 */
-	void setMomentumTermParameter (double factor);
+    /** Setter for the momentum term parameter.
+     *
+     * @param factor Value between 0.5 and 0.9.
+     */
+    void setMomentumTermParameter (double factor);
 
 protected:
-	/** Test expected output vector, used for the learning process. For
-	 * simple propagation it is not needed.
-	 */
-	vector<double>	test_output;
+    /** Test expected output vector, used for the learning process. For
+     * simple propagation it is not needed.
+     */
+    vector<double>  test_output;
 
-	/** Left-to-right list of layers within the network. Must be at least
-	 * two (one input and one output layer), but can be arbitrary large.
-	 * More than four layers does not archive any improvement of the
-	 * network capabilities, though.
-	 */
-	vector<PerceptronLayer *>	layers;
+    /** Left-to-right list of layers within the network. Must be at least
+     * two (one input and one output layer), but can be arbitrary large.
+     * More than four layers does not archive any improvement of the
+     * network capabilities, though.
+     */
+    vector<PerceptronLayer *>   layers;
 
-	/** Generic learn parameter epsilon, should be between 0.05 and 0.5.
-	 */
-	double		epsilon;
+    /** Generic learn parameter epsilon, should be between 0.05 and 0.5.
+     */
+    double      epsilon;
 
-	/** Test tolerance parameter, should be between 0.0 and 0.2.
-	 */
-	double		test_tolerance;
+    /** Test tolerance parameter, should be between 0.0 and 0.2.
+     */
+    double      test_tolerance;
 
-	/** Weight decay parameter, should be between 0.005 and 0.03.
-	 */
-	double		weight_decay;
+    /** Weight decay parameter, should be between 0.005 and 0.03.
+     */
+    double      weight_decay;
 
-	/** Momentum term parameter, should be between 0.5 and 0.9.
-	 */
-	double		momentum_term;
+    /** Momentum term parameter, should be between 0.5 and 0.9.
+     */
+    double      momentum_term;
 };
 
 #endif
