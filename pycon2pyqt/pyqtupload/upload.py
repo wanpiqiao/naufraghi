00b005b0d84ca51b5e56495ca0b21c9f6b5b1d95
lmancini80 <lmancini80@5ff7f63e-6833-0410-825b-1303a3858bc7>
2008-05-05 21:56:52 +0000

    Simple ftp uploader

---
+++ pycon2pyqt/pyqtupload/upload.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import os
+import sys
+import time
+import math
+import random
+from ftplib import FTP
+
+from PyQt4.Qt import *
+from PyQt4 import uic
+
+
+class UploadConfig(QWidget):
+    def __init__(self, *args):
+        QWidget.__init__(self, *args)
+        uic.loadUi("upload.ui", self)
+    def getConfig(self):
+        user, ftp = str(self.ledit_ftp.text()).rsplit("@", 1)
+        http = str(self.ledit_http.text())
+        pwd = str(self.ledit_pwd.text())
+        return locals()
+
+class Dropper(QLabel):
+    def __init__(self, *args):
+        QLabel.__init__(self, *args)
+        self.setAcceptDrops(True)
+        self.setText("Drop files here")
+    # Necessario accettare gli eventi di enter drag e move drag
+    def dragEnterEvent(self, event):
+        event.acceptProposedAction()
+    def dragMoveEvent(self, event):
+        event.acceptProposedAction()
+    def dropEvent(self, event):
+        event.acceptProposedAction()
+        # L'evento di drop contiene le informazioni a proposito dell'oggetto
+        if event.mimeData().hasUrls():
+            filepath = [unicode(url.toLocalFile()) for url in event.mimeData().urls()][0]
+            self.setText(filepath)
+            self.emit(SIGNAL("upload"), filepath)
+
+class Uploader(QWidget):
+    def __init__(self, *args):
+        QWidget.__init__(self, *args)
+        self.main = QVBoxLayout(self)
+        self.dropper = Dropper(None)
+        self.config = UploadConfig(None)
+        self.main.addWidget(self.dropper)
+        self.main.addWidget(self.config)
+        self.connect(self.dropper, SIGNAL("upload"), self.upload)
+    def upload(self, filepath):
+        print filepath
+        config = self.config.getConfig()
+        ftp = FTP(config["ftp"], config["user"], config["pwd"])
+        self.dropper.setText("Uploading '%s'..." % filepath)
+        print ftp.retrlines("LIST")
+        ftp.storbinary('STOR '+os.path.basename(filepath), open(filepath))
+        print ftp.retrlines("LIST")
+        self.dropper.setText(os.path.join(config["http"], os.path.basename(filepath)))
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    todo = Uploader()
+    todo.show()
+    sys.exit(app.exec_())
