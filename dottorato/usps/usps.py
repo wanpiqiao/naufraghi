a621ab499d3291926760d2d8b2d92c156aeec498
matteo.bertini <matteo.bertini@5ff7f63e-6833-0410-825b-1303a3858bc7>
2008-05-06 15:44:07 +0000

    Add usps dataset

---
+++ dottorato/usps/usps.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# Copyright (C) 2008 Matteo Bertini
+
+import os
+import sys
+
+os.chdir(os.path.dirname(__file__))
+
+sys.path.append("../")
+
+from bplnn import *
+
+def load_data(filename):
+    trace("Loading '%s'" % filename)
+    data = []
+    for line in open(filename).readlines():
+        row = map(int, line.split(","))
+        stroke, number = row[:-1], row[-1]
+        # normalize inputs
+        inputs = [float(i)/100 for i in stroke]
+        # digitalize outputs
+        targets = [int(i == number) for i in range(10)]
+        data.append([inputs, targets])
+    print stats(data)
+    print "-"*70
+    return data
+
+def run():
+    trace("PenDigits dataset", "#")
+    patterns = load_data("pendigits.tra")
+    n_in = len(patterns[0][0])
+    n_out = len(patterns[0][1])
+    net = DeepNetwork([n_in, n_in/2, n_in/4, n_out], auto_mode="input")
+    net.train(patterns, 1000)
+    print net
+    test_patterns = load_data("pendigits.tes")
+    net.test(test_patterns)
+    print net.dump()
+
+
+
+if __name__=="__main__":
+    run()
