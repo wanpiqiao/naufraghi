73cb74b189fe4cbf5a30a7b84fa940fa55921bb9
lmancini80 <lmancini80@5ff7f63e-6833-0410-825b-1303a3858bc7>
2008-05-08 14:46:59 +0000

    Qt stylesheets demo

---
+++ pycon2pyqt/pyqtcss/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import sys
+import time
+import math
+import random
+from PyQt4.Qt import *
+from PyQt4 import uic
+
+class SimpleForm(QDialog):
+    def __init__(self, *args):
+        QDialog.__init__(self, *args)
+        uic.loadUi("simpleform.ui", self)
+    
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    todo = SimpleForm()
+    todo.show()
+    sys.exit(app.exec_())
