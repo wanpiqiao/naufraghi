f9e9ae2d45ec80b40b95cc6434976fc89c19cc0e
matteo.bertini <matteo.bertini@5ff7f63e-6833-0410-825b-1303a3858bc7>
2008-05-22 15:00:23 +0000

    Use new Layer

--- dottorato/usps/usps.py
+++ dottorato/usps/usps.py
@@ -12,41 +12,35 @@ sys.path.append("../")
 
 from bplnn import *
 
-def load_data(filename):
+def load_patterns(filename):
     trace("Loading '%s'" % filename)
-    data = []
+    inputs = []
+    targets = []
     for line in gzip.open(filename).readlines():
         row = line.strip().split(" ")
         number, picture = int(row[0]), map(float, row[1:])
         # normalize inputs
-        inputs = picture # already [-1, 1]
+        inputs.append(picture) # already [-1, 1]
         # digitalize outputs
-        targets = [(i == number) and 1 or -1 for i in range(10)]
-        data.append([vector(inputs), vector(targets)])
-    print stats(data)
+        targets.append([(i == number) and 1 or -1 for i in range(10)])
+    print stats(inputs, targets)
     print "-"*70
-    return data
+    return np.mat(inputs), np.mat(targets)
 
 def run():
     trace("USPS dataset", "#")
-    patterns = load_data("zip.train.gz")
-    n_in = len(patterns[0][0])
-    n_out = len(patterns[0][1])
-    net = DeepNetwork([n_in, n_in/2, n_in/4, n_out], auto_mode="step")
+    inputs, targets = load_patterns("zip.train.gz")
+    test_inputs, test_targets = load_patterns("zip.test.gz")
+    n_in = inputs.shape[1]
+    n_out = targets.shape[1]
+    net = DeepNetwork([n_in, 500, 250, 100, n_out])
     print net
     trace("AutoTrain")
-    for i in range(1):
-        net.prepare(patterns, 100, 0.05)
-    trace("Test1")
-    test_patterns = load_data("zip.test.gz")
-    net.test(test_patterns)
+    net.prepare(inputs, 100, 0.05)
+    net.test(test_inputs, test_targets)
     trace("FineTrain")
-    net.train(patterns, 1000)
-    trace("Test2")
-    test_patterns = load_data("zip.test.gz")
-    net.test(test_patterns)
-    print net.dump()
-
+    net.train(inputs, targets, 100)
+    net.test(test_inputs, test_targets)
 
 
 if __name__=="__main__":
